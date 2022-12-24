6810e5d405998937936ff4733c81b00e16bbef46
Komyak <117581899+KomyakDevelopment@users.noreply.github.com>
2022-12-24 03:31:43 +0100

    Added files
    Script configuration.
    Resource lua
    And gitattributes has been added.

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config = {
+    ["ped"] = { -- Do not edit.
+        ["name"] = "Name",
+        ["pos"] = vector3(1878.5583, 476.4361, 171.6042), -- Where the ped will stand. X.Y.Z
+        ["heading"] = 63.1826, -- What direction the ped will stand.
+        ["hash"] = "mp_f_execpa_02", -- Get the hash here https://docs.fivem.net/docs/game-references/ped-models/
+    },
+
+}
